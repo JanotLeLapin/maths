c05c26ec3c415b884d6b6b5a574c830431a85e4c
Joseph <joseph300905@gmail.com>
2020-12-01 15:34:35 +0100

    Move random function to util

--- src/algorithmes.ts
+++ src/algorithmes.ts
@@ -8,12 +8,3 @@ export const exo1 = (z: number) => {
 export const f = (x: number) => {
     return 5 * x - x ** 2;
 };
-
-/**
- * Returns a random number between delimited values.
- * @param {number} from The minimum value.
- * @param {number} to The maximum value.
- */
-export const random = (from: number, to: number) => {
-    return Math.round(Math.random() * (to - from)) + from;
-};
--- src/util.ts
+++ src/util.ts
@@ -15,3 +15,12 @@ export const dot = (a: number[], b: number[]) => {
     }
     return a;
 };
+
+/**
+ * Returns a random number between delimited values.
+ * @param {number} from The minimum value.
+ * @param {number} to The maximum value.
+ */
+export const random = (from: number, to: number) => {
+    return Math.round(Math.random() * (to - from)) + from;
+};
