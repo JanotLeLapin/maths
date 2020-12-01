da76cdad0259f5d71a2429d139f013d45dd0242a
JanotLeLapin <51287094+JanotLeLapin@users.noreply.github.com>
2020-12-01 11:39:54 +0100

    Create random function

--- src/algorithmes.ts
+++ src/algorithmes.ts
@@ -9,3 +9,11 @@ export const f = (x: number) => {
     return 5 * x - x ** 2;
 };
 
+/**
+ * Returns a random number between delimited values.
+ * @param {number} from The minimum value.
+ * @param {number} to The maximum value.
+ */
+export const random = (from: number, to: number) => {
+    return Math.round(Math.random() * (to - from)) + from;
+};
