bae36614ff9b1b6a3ff4d7358b0f4cc56ab8b4e9
JanotLeLapin <51287094+JanotLeLapin@users.noreply.github.com>
2020-12-01 11:40:10 +0100

    Test

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
-import Rectangle from './rectangle';
+import Triangle from './triangle';
+[2, 3.2, 5.6, 10.9].forEach(i => console.log(new Triangle(i, i + 1).area));
 
-const r = new Rectangle({
-    perimeter: 10,
-    width: 3,
-});
-
-console.log(r.height);
-
-import { exo1 } from './algorithmes';
+import { exo1, f, random } from './algorithmes';
 
 const indexes = ['x', 'y', 'z'];
 console.log(exo1(1.8).map((n, i) => indexes[i] + ': ' + n).join('\n'));
+
+console.log(f(4));
+for (let i = 0; i < 100; i++) {
+    console.log(random(1, 100));
+}
