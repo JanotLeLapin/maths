f8df5875502693b77aec5f0382af2e03fe4154c8
JanotLeLapin <51287094+JanotLeLapin@users.noreply.github.com>
2020-12-02 10:38:56 +0100

    Test

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
-import { dot } from './util';
+import { add, dot, transpose } from './util';
 
-console.log(dot([1, 2, 3], [3, 4, 5]));
+console.log(transpose([[1, 2, 3], [4, 5, 6]]));
+console.log(dot([1, 2, 3], [4, 5, 6]));
+console.log(add([1, 2, 3], [4, 5, 6]));
