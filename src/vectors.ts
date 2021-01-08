e93c16267bd392e7cf53b0c9b6aa24aecc4fa1c8
Joseph <joseph300905@gmail.com>
2021-01-08 16:58:06 +0100

    Move functions into their own files

---
+++ src/vectors.ts
@@ -0,0 +1,32 @@
+/**
+ * @description Returns the dot product of two vectors.
+ * @example ```js
+ * const u = [1, 2, 3];
+ * const v = [2, 3, 4];
+ * const w = dot(u, v);
+ * ```
+ */
+export const dot = (a: number[], b: number[]) => {
+    if (a.length != b.length) throw new RangeError('Invalid vector length: ' + a.length + ' != ' + b.length + '.');
+    let result = 0;
+    for (let i = 0; i < a.length; i++) {
+        result += a[i] *= b[i];
+    }
+    return result;
+};
+
+/**
+ * Returns the sum of two vectors.
+ * @example ```js
+ * const u = [1, 2, 3];
+ * const v = [2, 3, 4];
+ * const w = add(u, v);
+ * ```
+ */
+export const add = (a: number[], b: number[]) => {
+    if (a.length !== b.length) throw new RangeError(a.length + ' !== ' + b.length + '.');
+    for (let i = 0; i < a.length; i++) {
+        a[i] += b[i];
+    }
+    return a;
+};
