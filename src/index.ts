3acd28b2d3e291b67e25a340c76fa89bcf9f15a9
Joseph <joseph300905@gmail.com>
2021-01-08 16:58:47 +0100

    Replace old with new exercise

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,15 @@
-import { h } from './algorithmes';
-import Rectangle from './rectangle';
+import { add, sub, mul } from './vectors';
 
-console.log('\nEX 2\n')
-const y: number[] = [1, 2, 2.5, 3, 3.5, 4, 4.5];
-for (let i = 0; i < y.length; i++) {
-    console.log(y[i] + ': ' + new Rectangle({
-        perimeter: 10,
-        width: y[i]
-    }).area);
-}
+// Ex 2: 1
+const u = [2, -2];
+const v = [-1, Math.sqrt(2)];
+const w = [1 / Math.sqrt(3), 3];
 
-console.log('\nEX 3\n');
-for (let i = 0; i <= 5; i++) {
-    console.log(h(i));
-}
-import { add, dot, transpose } from './util';
+console.log(sub(u, v));
+console.log(add(sub(mul(2, v), u), w));
 
-console.log(transpose([[1, 2, 3], [4, 5, 6]]));
-console.log(dot([1, 2, 3], [4, 5, 6]));
-console.log(add([1, 2, 3], [4, 5, 6]));
+// Ex 2: 2
+const a = [6, 9];
+const b = [4, 2];
+
+console.log(add(a, b));
