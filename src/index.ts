3108f51e8a5b87469234850d999595aed1f39095
Joseph <joseph300905@gmail.com>
2021-01-11 14:38:24 +0100

    Create logUsedMemory function

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { logUsedMemory } from './util';
+
 import Vector from './vector';
 
 const u = new Vector(2, -2);
@@ -7,3 +9,5 @@ const w = new Vector(1 / Math.sqrt(3), 3);
 console.log('=== Ex 2 ===');
 console.log('u - v(' + u.sub(v).coordinates + ')')
 console.log('2v - u + w(' + v.mul(2).sub(u).add(w).coordinates + ')');
+
+logUsedMemory();
--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,5 @@
+import process from 'process';
+
 /**
  * @description Returns a random number between delimited values.
  * @param {number} from The minimum value.
@@ -6,3 +8,8 @@
 export const random = (from: number, to: number) => {
     return Math.round(Math.random() * (to - from)) + from;
 };
+
+/**
+ * @description Logs the memory usage on the current Node.JS process.
+ */
+export const logUsedMemory = () => console.log('Memory usage: ' + Math.round(process.memoryUsage().heapUsed / 1024 / 1024 * 100) / 100 + 'Mb');
