caf7d00ed15e35f6a406195a9f9786fda2f58b52
Joseph <joseph300905@gmail.com>
2021-01-11 14:09:35 +0100

    Use Vector class in ex 2

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,9 @@
-import { add, sub, mul } from './vectors';
+import Vector from './vector';
 
-// Ex 2: 1
-const u = [2, -2];
-const v = [-1, Math.sqrt(2)];
-const w = [1 / Math.sqrt(3), 3];
+const u = new Vector(2, -2);
+const v = new Vector(-1, Math.sqrt(2));
+const w = new Vector(1 / Math.sqrt(3), 3);
 
-console.log(sub(u, v));
-console.log(add(sub(mul(2, v), u), w));
-
-// Ex 2: 2
-const a = [6, 9];
-const b = [4, 2];
-
-console.log(add(a, b));
+console.log('=== Ex 2 ===');
+console.log('u - v(' + u.sub(v).coordinates + ')')
+console.log('2v - u + w(' + v.mul(2).sub(u).add(w).coordinates + ')');
