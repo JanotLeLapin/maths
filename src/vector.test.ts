4ea11d6c4e35069e5bd2604b2b53f0a7cf974fa5
Joseph <joseph300905@gmail.com>
2021-01-11 14:09:17 +0100

    Create Vector class

---
+++ src/vector.test.ts
@@ -0,0 +1,11 @@
+import Vector from './vector';
+
+test('Vector calculation chain', () => {
+    const u = new Vector(1, 2, 3);
+    const v = new Vector(2, 3, 4);
+    const w = new Vector(3, 4, 5);
+    const k = 2;
+
+    expect(u.add(v).sub(w).coordinates).toStrictEqual([0, 1, 2]);
+    expect(v.mul(k).add(u).dot(w)).toBe(102);
+});
---
+++ src/vector.ts
@@ -0,0 +1,44 @@
+import { add as a, sub as s, mul as m, dot as d } from './vectors';
+
+export default class Vector {
+    private c: number[];
+
+    /**
+     * @description A vector.
+     * @param {number[]} coordinates The coordinates of the vector.
+     */
+    constructor(...coordinates: number[]) {
+        this.c = coordinates;
+    };
+
+    /**
+     * @description The coordinates of the vector.
+     */
+    get coordinates() {
+        return this.c;
+    };
+
+    /**
+     * @description Add this vector with another one.
+     * @param {Vector} v The vector to add this vector with
+     */
+    add = (v: Vector) => new Vector(...a(this.c, v.c));
+
+    /**
+     * @description Subtract this vector with another one.
+     * @param {Vector} v The vector to subtract this vector with
+     */
+    sub = (v: Vector) => new Vector(...s(this.c, v.c));
+
+    /**
+     * @description Multiply this vector with a scalar.
+     * @param {number} v The scalar to multiply this vector with
+     */
+    mul = (k: number) => new Vector(...m(k, this.c));
+
+    /**
+     * @description Multiply this vector with another one.
+     * @param {Vector} v The vector to multiply this vector with
+     */
+    dot = (v: Vector) => d(this.c, v.c);
+};
