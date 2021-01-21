b275416f4d4ca02316c3370ceca5a1e09cfe131c
JanotLeLapin <joseph300905@gmail.com>
2021-01-21 16:06:58 +0100

    Replace exercices

--- src/index.ts
+++ src/index.ts
@@ -2,41 +2,19 @@ import { logUsedMemory } from './util';
 
 import Vector from './vector';
 
-const exemple5 = () => {
-    const a = new Vector(1, 2);
-    const b = new Vector(-3, 1);
-    const c = new Vector(3, -1);
+const exemple7 = () => {
+    const a = new Vector(-1, 1);
+    const b = new Vector(3, 2);
+    const c = new Vector(-2, -3);
+    const d = new Vector(6, -1);
+    const e = new Vector(5, 0);
 
     const ab = Vector.join(a, b);
-    const ac = Vector.join(a, c);
-    const bc = Vector.join(b, c);
+    const cd = Vector.join(c, d);
 
-    console.log('=== Example 5 ===');
-    console.log('M(' + ab.middle().coordinates + '), ab(' + ab.coordinates + ')');
-    console.log('N(' + ac.middle().coordinates + '), ac(' + ac.coordinates + ')');
-    console.log('P(' + bc.middle().coordinates + '), bc(' + bc.coordinates + ')');
-};
+    console.log(ab.isColinear(cd));
+}
 
-const exemple6 = () => {
-    const a = new Vector(-2, 1);
-    const b = new Vector(4, 2);
-    const c = new Vector(1/2, -2);
-
-    const ab = Vector.join(a, b);
-    const ac = Vector.join(a, c);
-    const bc = Vector.join(b, c);
-
-    console.log('=== Exercice 6 ===');
-
-    console.log('M(' + ab.middle().coordinates + '), ab(' + ab.coordinates + ')');
-    console.log('N(' + ac.middle().coordinates + '), ac(' + ac.coordinates + ')');
-    console.log('P(' + bc.middle().coordinates + '), bc(' + bc.coordinates + ')');
-
-    const s = new Vector(7, 9);
-    const t = new Vector(3, 12);
-    console.log('Coordonnées du milieu du segment ST : (' + Vector.join(s, t).coordinates + ').');
-};
-
-exemple6();
+exemple7();
 
 logUsedMemory();
