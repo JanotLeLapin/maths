8e6cbf1cf81123146aecda55709c0bc871db3842
JanotLeLapin <joseph300905@gmail.com>
2021-01-21 13:53:16 +0100

    Write examples in vector docstrings

--- src/vector.ts
+++ src/vector.ts
@@ -20,7 +20,14 @@ export default class Vector {
 
     /**
      * @description Add this vector with another one.
-     * @param {Vector} v The vector to add this vector with
+     * @param {Vector} v The vector to add this vector with.
+     * @example ```ts
+     * const u = new Vector(1, 2, 3);
+     * const v = new Vector(2, 3, 4);
+     * const w = u.add(v);
+     *
+     * console.log(w.coordinates); // [3, 5, 7]
+     * ```
      */
     add(v: Vector) {
         return new Vector(...a(this.c, v.c));
@@ -28,7 +35,14 @@ export default class Vector {
 
     /**
      * @description Subtract this vector with another one.
-     * @param {Vector} v The vector to subtract this vector with
+     * @param {Vector} v The vector to subtract this vector with.
+     * @example ```ts
+     * const u = new Vector(1, 2, 3);
+     * const v = new Vector(2, 3, 4);
+     * const w = u.sub(v);
+     *
+     * console.log(w.coordinates); // [-1, -1, -1]
+     * ```
      */
     sub(v: Vector) {
         return new Vector(...s(this.c, v.c));
@@ -36,7 +50,14 @@ export default class Vector {
 
     /**
      * @description Multiply this vector with a scalar.
-     * @param {number} v The scalar to multiply this vector with
+     * @param {number} v The scalar to multiply this vector with.
+     * @example ```ts
+     * const u = new Vector(1, 2, 3);
+     * const k = 2;
+     * const w = u.mul(k);
+     *
+     * console.log(w.coordinates); // [2, 4, 6]
+     * ```
      */
     mul(k: number) {
         return new Vector(...m(k, this.c));
@@ -44,7 +65,14 @@ export default class Vector {
 
     /**
      * @description Multiply this vector with another one.
-     * @param {Vector} v The vector to multiply this vector with
+     * @param {Vector} v The vector to multiply this vector with.
+     * @example ```ts
+     * const u = new Vector(1, 2, 3);
+     * const v = new Vector(2, 3, 4);
+     * const w = u.add(v);
+     *
+     * console.log(w.coordinates); // 20
+     * ```
      */
     dot(v: Vector) {
         return d(this.c, v.c);
@@ -52,6 +80,10 @@ export default class Vector {
 
     /**
      * @description Returns the middle of this vector.
+     * @example ```ts
+     * const u = new Vector(1, 2, 3);
+     *
+     * console.log(u.middle().coordinates); // [0.5, 1, 1.5]
      */
     middle() {
         return this.mul(0.5);
@@ -60,6 +92,13 @@ export default class Vector {
     /**
      * @description Joins an array of vectors.
      * @param {Vector[]} v The vectors to join together.
+     * @example ```ts
+     * const u = new Vector(1, 2, 3);
+     * const v = new Vector(2, 3, 4);
+     * const w = new Vector(3, 4, 5);
+     *
+     * console.log(Vector.join(u, v, w).coordinates); // [0, -1, -2];
+     * ```
      */
     static join(...v: Vector[]) {
         let result = v[v.length - 1].c;
