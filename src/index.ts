e55c8c23585fb4624908d5c116cc3a69a1c968ac
JanotLeLapin <joseph300905@gmail.com>
2021-01-21 10:49:32 +0100

    Use Vector class instead of Point class

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,15 @@
 import { logUsedMemory } from './util';
 
 import Vector from './vector';
-import Point from './point';
 
 const exemple5 = () => {
-    const a = new Point(1, 2);
-    const b = new Point(-3, 1);
-    const c = new Point(3, -1);
+    const a = new Vector(1, 2);
+    const b = new Vector(-3, 1);
+    const c = new Vector(3, -1);
 
-    const ab = Point.toVector(a, b);
-    const ac = Point.toVector(a, c);
-    const bc = Point.toVector(b, c);
+    const ab = Vector.join(a, b);
+    const ac = Vector.join(a, c);
+    const bc = Vector.join(b, c);
 
     console.log('=== Example 5 ===');
     console.log('M(' + ab.middle().coordinates + '), ab(' + ab.coordinates + ')');
@@ -19,13 +18,13 @@ const exemple5 = () => {
 };
 
 const exemple6 = () => {
-    const a = new Point(-2, 1);
-    const b = new Point(4, 2);
-    const c = new Point(1/2, -2);
+    const a = new Vector(-2, 1);
+    const b = new Vector(4, 2);
+    const c = new Vector(1/2, -2);
 
-    const ab = Point.toVector(a, b);
-    const ac = Point.toVector(a, c);
-    const bc = Point.toVector(b, c);
+    const ab = Vector.join(a, b);
+    const ac = Vector.join(a, c);
+    const bc = Vector.join(b, c);
 
     console.log('=== Exercice 6 ===');
 
@@ -33,9 +32,9 @@ const exemple6 = () => {
     console.log('N(' + ac.middle().coordinates + '), ac(' + ac.coordinates + ')');
     console.log('P(' + bc.middle().coordinates + '), bc(' + bc.coordinates + ')');
 
-    const s = new Point(7, 9);
-    const t = new Point(3, 12);
-    console.log('Coordonnées du milieu du segment ST : (' + Point.toVector(s, t).coordinates + ').');
+    const s = new Vector(7, 9);
+    const t = new Vector(3, 12);
+    console.log('Coordonnées du milieu du segment ST : (' + Vector.join(s, t).coordinates + ').');
 };
 
 exemple6();
