db116b7cafe63b6eaf0205971e415e7281727f4e
JanotLeLapin <joseph300905@gmail.com>
2021-01-21 10:49:03 +0100

    Make arrow functions regular functions

--- src/vector.ts
+++ src/vector.ts
@@ -22,29 +22,41 @@ export default class Vector {
      * @description Add this vector with another one.
      * @param {Vector} v The vector to add this vector with
      */
-    add = (v: Vector) => new Vector(...a(this.c, v.c));
+    add(v: Vector) {
+        return new Vector(...a(this.c, v.c));
+    };
 
     /**
      * @description Subtract this vector with another one.
      * @param {Vector} v The vector to subtract this vector with
      */
-    sub = (v: Vector) => new Vector(...s(this.c, v.c));
+    sub(v: Vector) {
+        return new Vector(...s(this.c, v.c));
+    };
 
     /**
      * @description Multiply this vector with a scalar.
      * @param {number} v The scalar to multiply this vector with
      */
-    mul = (k: number) => new Vector(...m(k, this.c));
+    mul(k: number) {
+        return new Vector(...m(k, this.c));
+    };
 
     /**
      * @description Multiply this vector with another one.
      * @param {Vector} v The vector to multiply this vector with
      */
-    dot = (v: Vector) => d(this.c, v.c);
+    dot(v: Vector) {
+        return d(this.c, v.c);
+    };
 
     /**
      * @description Returns the middle of this vector.
      */
+    middle() {
+        return this.mul(0.5);
+    };
+
     /**
      * @description Joins two vectors.
      * @param {Vector[]} v The vector to join this vector with.
